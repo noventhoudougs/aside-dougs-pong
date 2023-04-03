b98771807a18dede78a8fb904659256ffe795d69
lucasbouvarel <lucas.bouvarel@gmail.com>
2023-04-03 16:46:24 +0200

    add players vue

---
+++ front/pages/Players.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="container">
+    <div>
+      <h1>Liste des joueurs</h1>
+      <ul>
+        <li v-for="player in players" :key="player.email">
+          {{ player.firstname }} {{player.lastname}}
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async created() {
+    const players = (await this.$axios.get('/users')).data.results;
+    this.players = players;
+  },
+  data() {
+    return {
+      players: [],
+    }
+  },
+  })
+</script>
