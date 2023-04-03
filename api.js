fbf599d7d7f2058b95c59304f0ba5b7ad0e3b072
noventhoudougs <noven.thou@dougs.fr>
2023-04-03 12:20:29 +0200

    connect db and update score

--- api.js
+++ api.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const path = require('path');
+const { Pool } = require('pg');
 
 const api = express();
+const pool = new Pool({
+    user: 'dougs',
+    host: 'localhost',
+    database: 'dougs-aside-pong',
+    password: '',
+    port: 5432,
+  });
+
+api.get('/users', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+});
+
+api.post('/users', async (req, res) => {
+    const client = await pool.connect();
+    const email = req.query.email;
+    const score = Number(req.query.score);
+    const results = await client.query(`SELECT score FROM users WHERE email = '${email}'`);
+    const updatingScore = results.rows[0].score + score;
+    const result = await client.query(`UPDATE users SET score = '${updatingScore}' WHERE email = '${email}'`);
+    res.send(result);
+    client.release();
+});
 
 api.use(express.static(path.join(__dirname, 'public')));
 
-api.use('/*', (req, res) => {
+api.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
